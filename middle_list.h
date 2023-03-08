86c2e7667e79275b57a868c9fe0ea2d7ddfd5c71
Belovolov Victor <112112051+BictorVelovolov@users.noreply.github.com>
2023-03-08 21:27:36 +0300

    Update middle_list.h

--- middle_list.h
+++ middle_list.h
@@ -4,7 +4,10 @@
 #include <string>
 using namespace std;
 
-vector <char> itc_strlist(string str);
+vector <char> itc_strlist(string str); //Number 1
+string itc_join(vector <char> lst, string sep); //Number 2
+string itc_rmstrspc(string str); //Number 3
+string itc_rmstrchar(string str, string les); //Number 4
 
 
 
