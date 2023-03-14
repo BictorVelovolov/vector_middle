7a03b7b35dfe76a30bb347643a9c763458b7576f
Belovolov Victor <112112051+BictorVelovolov@users.noreply.github.com>
2023-03-14 23:24:08 +0300

    Update midvec2.cpp

--- midvec2.cpp
+++ midvec2.cpp
@@ -6,7 +6,6 @@ using namespace std;
 
 long itc_sum_even_lst(const vector <int> &lst){
     long sum;
-    sum = 0;
     for (int i = 1; i < lst.size(); i += 2)
         sum += lst[i];
     return sum;
@@ -14,7 +13,6 @@ long itc_sum_even_lst(const vector <int> &lst){
 
 long itc_sum_even_part_lst(const vector <int> &lst){
     long sum;
-    sum = 0;
     for (int i = 0; i < lst.size(); i++)
         if(lst[i] % 2 == 0)
             sum += lst[i];
@@ -23,12 +21,21 @@ long itc_sum_even_part_lst(const vector <int> &lst){
 
 long itc_sum_not_even_part_lst(const vector <int> &lst){
     long sum;
-    sum = 0;
     for (int i = 0; i < lst.size(); i++)
         if(lst[i] % 2 != 0)
             sum += lst[i];
     return sum;
 }
+
+void itc_odd_even_separator_lst(const vector <int> &lst, vector <int> &lst1, vector <int> &lst2){
+    for(int i = 0; i < lst.size(); i++){
+        if(lst[i] % 2 == 0)
+            lst1.push_back(lst[i]);
+        else
+            lst2.push_back(lst[i]);
+    }
+}
+
 
 void itc_pos_neg_separator_lst(const vector <int> lst, vector <int> lst1, vector <int> lst2, vector <int> lst3){
     int ch;
@@ -44,32 +51,29 @@ void itc_pos_neg_separator_lst(const vector <int> lst, vector <int> lst1, vector
 }
 
 void itc_odd_even_analysis_lst(const vector <int> lst){
+    vector <int> evlst;
+    vector <int> nevlst;
     int max2, max1, min2, min1, ek, nek;
-    ek = 0;
-    nek = 0;
     max2 = -2147483648;
     min2 = 2147483647;
     max1 = max2;
     min1 = min2;
-    for (int i = 0; i < lst.size(); i++){
-        if(lst[i] % 2 == 0){
-            ek++;
-            if (lst[i] > max2)
-                max2 = lst[i];
-            if (lst[i] < min2)
-                min2 = lst[i];
+    itc_odd_even_separator_lst(lst, evlst, nevlst);
+    for(ek = 0; ek < evlst.size(); ek++){
+            if(evlst[ek] > max2)
+                max2 = evlst[ek];
+            if(evlst[ek] < min2)
+                min2 = evlst[ek];
         }
-       if(lst[i] % 2 == 1 || lst[i] % 2 == -1){
-            nek++;
-            if (lst[i] > max1)
-                max1 = lst[i];
-            if (lst[i] < min1)
-                min1 = lst[i];
+    for(nek = 0; nek < nevlst.size(); nek++){
+            if(nevlst[nek] > max1)
+                max1 = nevlst[nek];
+            if(nevlst[nek] < min1)
+                min1 = nevlst[nek];
         }
-    }
     cout << "[" << lst[0];
     for(int i = 1; i < lst.size(); i++)
         cout << "," << lst[i];
-    cout << "] --> \nvector analysis: \neven parts kolichestvo:" << ek << endl << "maximum even: " << max2 << endl << "minimum even: " << min2 << endl << "summa of even:" << itc_sum_even_part_lst(lst);
-    cout << endl << "\nnot even parts kolichestvo:" << nek << endl << "maximum not even: " << max1 << endl << "minimum  not even: " << min1 << endl << "summa of not even:" << itc_sum_not_even_part_lst(lst);
+    cout << "] --> \nvector analysis: \neven parts kolichestvo:" << ek << endl << "maximum even: " << max2 << endl << "minimum even: " << min2 << endl << "summa of even:" << itc_sumlst(evlst);
+    cout << endl << "\nnot even parts kolichestvo:" << nek << endl << "maximum not even: " << max1 << endl << "minimum  not even: " << min1 << endl << "summa of not even:" << itc_sumlst(nevlst);
 }
