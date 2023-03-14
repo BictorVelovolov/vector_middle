bec8dcdded79b50ec7fa47908a3d885d882f530e
Belovolov Victor <112112051+BictorVelovolov@users.noreply.github.com>
2023-03-14 17:10:49 +0300

    Update midvec1.cpp

--- midvec1.cpp
+++ midvec1.cpp
@@ -4,7 +4,7 @@
 #include "middle_list.h"
 using namespace std;
 
-vector <char> itc_strlist(string str){
+vector <char> itc_strtlist(string str){
     vector <char> strok;
     for (int i = 0; i < str.size(); i++)
         strok.push_back(str[i]);
